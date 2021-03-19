50f6cb281d1d41e9534d542b643c5fba2ec6ffc4
warlocker64 <70263816+warlocker64@users.noreply.github.com>
2021-03-19 22:38:25 +0100

    LEs GO

---
+++ Test/Test2.py
@@ -0,0 +1,5 @@
+amount = 10
+number = 0
+for time in range(amount):
+    number += 1
+    print(f"{number} Abracadabra")
---
+++ Test/Tests.py
@@ -0,0 +1,5 @@
+N = int(input(""))
+number = 0
+for time in range(N):
+    number += 1
+    print(f"{number} Abracadabra")
